a3347cf27e2c7c3bc9cfa4ccb493a4342a79c9b6
Aryapawar123 <arya.pawar0611@gmail.com>
2026-03-24 15:48:03 +0530

    Added Firebase integration

---
+++ arthsetu/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJS3Ab7lq5RkyOTmrkjvyW7RMHCTy39Oo",
+  authDomain: "arthsetu-21496.firebaseapp.com",
+  projectId: "arthsetu-21496",
+  storageBucket: "arthsetu-21496.firebasestorage.app",
+  messagingSenderId: "1017304784921",
+  appId: "1:1017304784921:web:72eb5155aa294010000a88",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
